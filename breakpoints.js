fc0571c8e88337f05e668ab9335eec8eb61893dd
Carlos Daiha Basna <carlos.daiha-basna@capgemini.com>
2021-12-01 08:43:50 +0100

    Breakpoint modified and HTML file created

--- breakpoints.js
+++ breakpoints.js
@@ -1,7 +1,10 @@
-console.log("Mensaje antes del breakpoint");
+while (true) {
+    console.log("Mensaje antes del breakpoint");
+    break;
 
-/* Se añade un breakpoint justo después del primer mensaje
- * De esta manera no se imprime por consola el segundo mensaje
- */
-
-console.log("Mensaje después del breakpoint");
+    /* Se añade un breakpoint justo después del primer mensaje
+    * De esta manera no se imprime por consola el segundo mensaje
+    */
+   
+    console.log("Mensaje después del breakpoint");
+}
