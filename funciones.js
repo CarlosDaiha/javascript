dbb012bea9ae9c873355054f7324664f553297a0
Carlos Daiha Basna <carlos.daiha-basna@capgemini.com>
2021-12-01 12:19:53 +0100

    Añadidas más funciones

--- funciones.js
+++ funciones.js
@@ -1,4 +1,43 @@
 function square(number) {
   return number * number;
 }
-console.log(square(2));
+
+function operaciones(number1, number2) {
+  console.log(number1-number2);
+  console.log(number1+number2);
+  console.log(number1*number2);
+  console.log(number1/number2);
+  return;
+}
+
+function areaTriangulo(base, altura) {
+  return base*altura/2;
+}
+
+function perimetroRectangulo(longitud, ancho) {
+  return (2*longitud)+(2*ancho);
+}
+
+let empleados = [{"Nombre": "Pepito", "Sueldo": 1666},
+{"Nombre": "Fulanito", "Sueldo": 1500},
+{"Nombre": "Menganito", "Sueldo": 1800},
+{"Nombre": "Cristiano", "Sueldo": 5000},
+{"Nombre": "Messi", "Sueldo": 6776}];
+function calcularSueldo(array) {
+  let empleado, sueldo = 0;
+  for (let i = 0; i < array.length; i++) {
+    sueldo = Math.max(sueldo, array[i].Sueldo);
+  }
+  array.forEach(element => {
+    if (sueldo === element.Sueldo) empleado = element;
+  });
+  return empleado;
+}
+
+function repartirHospital(presupuesto) {
+  let pediatria, urgencia, traumatologia;
+  pediatria = presupuesto*0.37;
+  urgencia = presupuesto*0.42;
+  traumatologia = presupuesto*0.21;
+  return ["Pediatría: "+pediatria, "Urgencia: "+urgencia,"Traumatología: "+traumatologia];
+}
