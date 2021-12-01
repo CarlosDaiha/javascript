3f3138d7ac7605ff54dc4cd7474beebc773e3e3d
Carlos Daiha Basna <carlos.daiha-basna@capgemini.com>
2021-12-01 09:52:40 +0100

    Ampliación de información en los arreglos

--- arreglos.js
+++ arreglos.js
@@ -1,3 +1,23 @@
 const MY_ARRAY = ['HTML','CSS'];
 MY_ARRAY.push('JAVASCRIPT');
 console.log(MY_ARRAY); // registra ['HTML','CSS','JAVASCRIPT'];
+
+let coffees = ['French Roast', 'Colombian', 'Kona'];
+
+//Recorre todos los elementos del array y los imprime por consola
+coffees.forEach(element => {
+    console.log(element);
+});
+
+coffees.unshift('Capuccino'); //Inserta al principio
+coffees.push('Mocaccino'); //Inserta al final
+console.log(coffees);
+
+let moca;
+moca = coffees.pop(); /* Quita el último elemento del array y, 
+opcionalmente, te lo guarda en la variable especificada */
+console.log(coffees);
+console.log(moca);
+
+coffees.splice(1,2); //Borra elementos específicos del array
+console.log(coffees);
--- arreglos_literales.js
+++ arreglos_literales.js
@@ -1,2 +1,18 @@
-let coffees = ['French Roast', 'Colombian', 'Kona'];
-console.log(coffees);
+let personaje = {
+    nombre: "Tony Stark",
+    nombreClave: "IronMan",
+    vivo: false,
+    edad: 40,
+    coordenadas: {
+        lat: 0.3232323,
+        lng: -23.939239
+    },
+    trajes: ['Mark I', 'Mark XXV', 'Hulkbuster'],
+    direccion: {
+        zip: '102212, 232323',
+        ubicacion: 'Malibu, California'
+    }
+};
+
+console.log(personaje); //Se muestra todo el objeto
+console.log(personaje.nombre); //Se muestra el valor de esa clave del objeto
