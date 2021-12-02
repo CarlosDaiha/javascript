1cd0faee0b588d20fcf1d92a99dbc382f355336e
Carlos Daiha Basna <carlos.daiha-basna@capgemini.com>
2021-12-02 11:59:21 +0100

    Interacción con formularios

--- funciones.js
+++ funciones.js
@@ -2,44 +2,43 @@ function square(number) {
   return number * number;
 }
 
-function operaciones(number1, number2) {
-  const sub = number1-number2;
-  const sum = number1+number2;
-  const mul = number1*number2;
-  const div = number1/number2;
-  return ["Resta: "+sub, "Suma: "+sum, "Multiplicación: "+mul, "División: "+div];
+function areaTriangulo() {
+  let base = document.getElementById("base").value;
+  let altura = document.getElementById("altura").value;
+  return document.getElementById("ej2").innerHTML = "El área del triángulo es "+Number(base*altura/2)+".";
 }
 
-function areaTriangulo(base, altura) {
-  return base*altura/2;
+function perimetroRectangulo() {
+  let longitud = document.getElementById("longitud").value;
+  let ancho = document.getElementById("ancho").value;
+  return document.getElementById("ej3").innerHTML = "El perímetro del rectángulo es "+Number((2*longitud)+(2*ancho))+".";
 }
 
-function perimetroRectangulo(longitud, ancho) {
-  return (2*longitud)+(2*ancho);
-}
+function calcularSueldo() {
+  const array = [{"Nombre": document.getElementById("nombre1").value, "Sueldo": document.getElementById("sueldo1").value},
+                {"Nombre": document.getElementById("nombre2").value, "Sueldo": document.getElementById("sueldo2").value},
+                {"Nombre": document.getElementById("nombre3").value, "Sueldo": document.getElementById("sueldo3").value},
+                {"Nombre": document.getElementById("nombre4").value, "Sueldo": document.getElementById("sueldo4").value},
+                {"Nombre": document.getElementById("nombre5").value, "Sueldo": document.getElementById("sueldo5").value}];
 
-const empleados = [{"Nombre": "Pepito", "Sueldo": 1666},
-{"Nombre": "Fulanito", "Sueldo": 1500},
-{"Nombre": "Menganito", "Sueldo": 1800},
-{"Nombre": "Messi", "Sueldo": 6776},
-{"Nombre": "Cristiano", "Sueldo": 5000}];
-function calcularSueldo(array) {
   let empleado, sueldo = 0;
   for (let i = 0; i < array.length; i++) {
-    sueldo = Math.max(sueldo, array[i].Sueldo);
+    sueldo = Math.max(sueldo, Number(array[i].Sueldo));
   }
   array.forEach(element => {
-    if (sueldo === element.Sueldo) empleado = element;
+    if (sueldo == element.Sueldo) empleado = element;
   });
-  return empleado;
+  return document.getElementById("ej4").innerHTML = "El empleado "+empleado.Nombre+" es el que más gana "+empleado.Sueldo+".";
 }
 
-function repartirHospital(presupuesto) {
+function repartirHospital() {
+  let presupuesto = Number(document.getElementById("presupuesto").value);
   let pediatria, urgencia, traumatologia;
   pediatria = presupuesto*0.37;
   urgencia = presupuesto*0.42;
   traumatologia = presupuesto*0.21;
-  return ["Pediatría: "+pediatria, "Urgencia: "+urgencia,"Traumatología: "+traumatologia];
+  txt = ["Pediatría: "+pediatria, " Urgencia: "+urgencia," Traumatología: "+traumatologia];
+  return document.getElementById("ej5").innerHTML = txt;
 }
 
 function getExtra() {
@@ -63,23 +62,25 @@ function esNavidad() {
   else return document.getElementById("navidad").innerHTML = "No es navidad aún...";
 }
 
-const personas = [{"Nombre": "Pepito", "Sexo": "M"},
-{"Nombre": "Fulanito", "Sexo": "M"},
-{"Nombre": "Menganito", "Sexo": "M"},
-{"Nombre": "Cristina", "Sexo": "F"},
-{"Nombre": "Marta", "Sexo": "F"},
-{"Nombre": "Sara", "Sexo": "F"},
-{"Nombre": "Soraya", "Sexo": "F"},
-{"Nombre": "Carlos", "Sexo": "M"},
-{"Nombre": "Almudena", "Sexo": "F"}];
-function porcentajes(alumnos) {
+function isXmas() {
+  const date = document.getElementById("fecha").value.toString();
+  let array = date.split("-");
+  if (array[1] == 12 && array[2] == 25) {
+    return document.getElementById("ej7").innerHTML = "¡Es Navidad!";
+  }
+  else return document.getElementById("ej7").innerHTML = "No es navidad aún...";
+}
+
+function porcentajes() {
   let hombres = 0, mujeres = 0;
+  const json = document.getElementById("dataset").value;
+  let alumnos = JSON.parse(json);
   alumnos.forEach(alumno => {
     if (alumno.Sexo === "M") hombres++;
     else mujeres++;
   });
-  return [{"Hombres":(hombres/alumnos.length*100).toFixed(2)+"%", 
-  "Mujeres":(mujeres/alumnos.length*100).toFixed(2)+"%"}];
+  txt = "Hombres: "+(hombres/alumnos.length*100).toFixed(2)+"%, Mujeres: "+(mujeres/alumnos.length*100).toFixed(2)+"%";
+  return document.getElementById("ej8").innerHTML = txt;
 }
 
 function getList() {
